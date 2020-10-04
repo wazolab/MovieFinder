1cc1399ae153bac480e3f59197000b742f6bbd2d
Noé Viricel <noe.viricel@wazo-lab.io>
2020-10-04 15:34:48 +0200

    Add package description in html head

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -1,3 +1,5 @@
+import pkg from "./package";
+
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
@@ -8,7 +10,7 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" }
+      { hid: "description", name: "description", content: pkg.description }
     ],
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
